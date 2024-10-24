4936f45df53d630f5e3d2ed46bdb5c91b8c94568
Tunar7 <tunarrsulov67@gmail.com>
2024-10-24 05:35:06 -0700

    Add files via upload

---
+++ Homework 2/homework 2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    char ad[] = "Tunar";
+    int yas = 17;
+    char ayinIlkHerfi = 'A';
+    printf("Mənim adım %s.\n", ad);
+    printf("Mənim %d yaşım var.\n", yas);
+    printf("Bu ay %c hərfi ilə başlayır.\n", ayinIlkHerfi);
+
+    return 0;
+}
