95fe22514b30f987c445c6d16693a881f71be9b6
Tunar7 <tunarrsulov67@gmail.com>
2024-10-24 10:57:23 -0700

    Add files via upload

---
+++ Homework 3/Untitled1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#define CONSTANT 10
+int main() {
+    int a = 5;
+    int b = 3;
+    int c = 2;
+    int sum = a + b + c;
+    printf("Sum: %d\n", sum);
+    printf("Constant: %d\n", CONSTANT);
+
+    a++;
+    printf("Incremented a: %d\n", a);
+    return 0;
+    }
